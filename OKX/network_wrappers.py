5d9830621ae8ce04d357f2fae5a1c34195192677
ageorge95 <arteni.george.daniel@gmail.com>
2023-11-16 15:13:27 +0200

    expanded the API_call class to properly handle get and post calls

--- OKX/network_wrappers.py
+++ OKX/network_wrappers.py
@@ -1,4 +1,4 @@
-from requests import get
+from requests import get, post
 from typing import AnyStr,\
     Dict
 from logging import getLogger
@@ -11,7 +11,7 @@ class API_call():
                  data: Dict,
                  headers: Dict = None,
                  max_retries: int = 1,
-                 call_method = get):
+                 call_method: [get, post] = get):
 
         self._log = getLogger()
         self.final_URL = base_url + '/' + added_url
@@ -27,12 +27,18 @@ class API_call():
             try:
                 self._log.info(f'Sending an API call to {self.final_URL} at retry attempt {current_retry + 1}/{self.max_retries}')
                 # remove parameters with no values
-                get_params = dict(filter(lambda _:_[1], {'url': self.final_URL,
-                                                         'params': self.data,
-                                                         'headers': self.headers,
-                                                         'timeout': (5*60,5*60)}.items()))
+                params = dict(filter(lambda _:_[1], {'url': self.final_URL,
+                                                     'headers': self.headers,
+                                                     'timeout': (5*60,5*60)}.items()))
+                if self.call_method is post:
+                    params['json'] = self.data
+                elif self.call_method is get:
+                    params['params'] = self.data
+                else:
+                    raise Exception(f'Unknown call method {self.call_method}')
+
                 return {'API_call_success': True,
-                        'data': self.call_method(**get_params).json()}
+                        'data': self.call_method(**params).json()}
             except:
                 self._log.error(f'Failed to send an API call to {self.final_URL} at retry attempt {current_retry + 1}/{self.max_retries}')
                 current_retry += 1
